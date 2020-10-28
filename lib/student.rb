e3634dc35a962b4ece5c5d9741856670bb0fcbf2
smassarsky <seth.massarsky@gmail.com>
2020-10-28 13:48:56 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,85 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    all = []
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+
+    DB[:conn].execute(sql).each{|row| all << new_from_db(row)}
+    all
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+    SQL
+
+    new_from_db(DB[:conn].execute(sql, name)[0])
+  end
+
+  def self.all_students_in_grade_9
+    all = []
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql).each{|row| all << new_from_db(row)}
+    all
+  end
+
+  def self.students_below_12th_grade
+    all = []
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade < 12
+    SQL
+
+    DB[:conn].execute(sql).each{|row| all << new_from_db(row)}
+    all
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    all = []
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, x).each{|row| all << new_from_db(row)}
+    all
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+
+    new_from_db(DB[:conn].execute(sql)[0])
+  end
+
+  def self.all_students_in_grade_X(x)
+    all = []
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade == ?
+    SQL
+
+    DB[:conn].execute(sql, x).each{|row| all << new_from_db(row)}
+    all
   end
   
   def save
